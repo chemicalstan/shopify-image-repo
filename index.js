870094b383f654a8a1b357e150fd65cafb7b897c
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-13 21:29:42 +0100

    Initialize db connection and initiate dotenv config

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+const dbUrl = process.env.MONGO_URI;
 // Initialize application
-const app = express()
+const app = express();
 
-
-
-
+// DB Connection
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch(err => console.log(err));
 
 // Listen
-app.listen(PORT, ()=>{
-    console(`Server running at ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running at port:${PORT}`);
+});
