cc214fc38960457de17fa59a2a3d856fdffa7edd
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-13 21:20:50 +0100

    Add tag property and set image url required to true in Image db schema

--- models/Image.js
+++ models/Image.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const imageSchema = new Schema({
-    image_url: String,
+    image_url: {
+        type: String,
+        required: true
+    },
     description: String,
+    tag: String,
     created_at: {
         type: Date,
         default: Date.now()
