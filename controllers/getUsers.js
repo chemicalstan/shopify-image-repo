b0490019d3a0606d261a481954429e0f1b0132fa
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-14 13:11:52 +0100

    Integrate get all users controller architecture

---
+++ controllers/getUsers.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const {status} = require('../helpers/status');
+
+/**
+ * Get all Users
+ * @param {object} req 
+ * @param {object} res 
+ * @returns {object} users array
+ */
+const getUsers = async (req, res)=>{
+    // Get all users from db
+    console.log('getting users...')
+}
