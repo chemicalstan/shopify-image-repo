aacb66ac83e887656d216bb93f85da8f0841c93e
Stanley Ebuka <Chemicalstan15@Gmail.Com>
2021-01-14 13:11:22 +0100

    Integrate get all images controller architecture

---
+++ controllers/getImages.js
@@ -0,0 +1,12 @@
+const mongooes = require('mongoose');
+const {status} = require('../helpers/status');
+module.exports = {
+/**
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} Images array
+ */
+    getImages: async (req, res)=>{
+        // Get all images from database
+    }
+}
